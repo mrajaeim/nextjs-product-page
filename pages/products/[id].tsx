1bf63a63997a4ca6da933822c0a10012ae4d52d3
Mohammad Javad Rajaei Monfared <mrajaeim.pro@gmail.com>
2022-11-14 14:38:20 +0330

    [feat] add select

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -13,6 +13,7 @@ import ProductOptionsWrapper from "../../components/product/ProductOptionsWrappe
 import ColorPicker from "../../components/product/ProductColor";
 import ProductImage from "../../components/product/ProductImage";
 import useColorPicker from "../../zustands/useColorPicker";
+import Select from "rc-select";
 
 const ProductPage: React.FC<{ product: Product }> = ({product}) => {
     const location = useLocation();
@@ -64,8 +65,22 @@ const ProductPage: React.FC<{ product: Product }> = ({product}) => {
                                 <ProductOption title={"COLOR"} className={"mb-4 w-full sm:w-full lg:w-1/3"}>
                                     {productColors.map((color, index) => <ColorPicker color={color} key={index}/>)}
                                 </ProductOption>
-                                <ProductOption title={"SIZE"} className={"mb-4 w-full sm:w-1/2 lg:w-1/3 lg:pl-4"}/>
-                                <ProductOption title={"QTY"} className={"mb-4 w-full sm:w-1/2 sm:pl-4 lg:w-1/3"}/>
+                                <ProductOption title={"SIZE"} className={"mb-4 w-full sm:w-1/2 lg:w-1/3 lg:pl-4"}>
+                                    <Select defaultValue={"(UK 7)"}>
+                                        <Select.Option value="(UK 7)">(UK 7)</Select.Option>
+                                        <Select.Option value="(UK 8)">(UK 8)</Select.Option>
+                                        <Select.Option value="(UK 9)">(UK 9)</Select.Option>
+                                        <Select.Option value="(UK 10)">(UK 10)</Select.Option>
+                                    </Select>
+                                </ProductOption>
+                                <ProductOption title={"QTY"} className={"mb-4 w-full sm:w-1/2 sm:pl-4 lg:w-1/3"}>
+                                    <Select defaultValue={"(1)"}>
+                                        <Select.Option value="(1)">(1)</Select.Option>
+                                        <Select.Option value="(2)">(2)</Select.Option>
+                                        <Select.Option value="(3)">(3)</Select.Option>
+                                        <Select.Option value="(4)">(4)</Select.Option>
+                                    </Select>
+                                </ProductOption>
                             </ProductOptionsWrapper>
                         </div>
                         <div className={"mt-10 flex justify-between"}>
